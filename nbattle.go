64914b090a0a7581161b4abe1d081186f7057ac4
Nathan Ogden <nathan@ogden.tech>
2026-05-11 12:52:27 -0400

    export important types in base module

--- nbattle.go
+++ nbattle.go
@@ -7,12 +7,19 @@ import (
 	"github.com/chompy/nbattle_go/internal/lua"
 )
 
+type Context *combat.Context
+type StatDef *combat.StatDef
+type EffectDef *combat.EffectDef
+type TriggerDef *combat.TriggerDef
+type Stat *combat.Stat
+type Combatant *combat.Combatant
+
 // New creates a new NBattle context.
-func New() *combat.Context {
+func New() Context {
 	return combat.New()
 }
 
 // NewLuaEffect creates a new effect definition from a Lua script.
-func NewLuaEffect(ctx *combat.Context, script io.Reader) (*combat.EffectDef, error) {
+func NewLuaEffect(ctx *combat.Context, script io.Reader) (EffectDef, error) {
 	return lua.NewEffect(ctx, script)
 }
